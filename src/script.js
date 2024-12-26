4dd90317f8de1b22490135682552153515370061
Sumanththumma <tsumanth3@gmail.com>
2024-12-26 14:23:13 +0530

    Added Time and Date objects
    minor push

---
+++ src/script.js
@@ -0,0 +1,39 @@
+const date = new Date();
+const timeanddate = document.getElementById('timeanddate')
+
+const hoursandminutes = () =>{
+    let minutes =  date.getMinutes();
+    let hours = date.getHours();
+    hours = hours > 12? hours-12 : hours;
+    hours = hours < 10? "0"+hours: hours;
+    minutes = minutes < 10? "0"+minutes: minutes;
+    let time = hours +":"+ minutes;
+    return time;
+}
+
+const ampm = () => {
+    let hours = date.getHours();
+    if(hours < 12){
+        return " AM"
+    }
+    else{
+        return " PM"
+    }
+}
+
+const day = () =>{
+    const dayarr = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    return dayarr[date.getDay()];
+}
+const tareekh = () =>{
+    let montharr = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+    month = montharr[date.getMonth()]
+    return date.getDate() +" "+ month;
+}
+
+
+setInterval(() => {
+    timeanddate.innerHTML = `<div id="time" class="font-semibold text-xl">${hoursandminutes()}<span id="ampm">${ampm()}</span>
+                <div class="text-sm font-normal" id="day">${day()}, ${tareekh()}</div>
+            </div>`
+}, 1000);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'day': "url('https://img.freepik.com/free-photo/sun-flare_74190-1494.jpg')",
+        'night': "url(https://images.pexels.com/photos/813269/pexels-photo-813269.jpeg?cs=srgb&dl=pexels-minan1398-813269.jpg&fm=jpg)"
+       
+      }
+    },
   },
   plugins: [],
 }
