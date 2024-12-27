322794f5f227e6d0f4e729a4d99a1e7da7988e91
Sumanththumma <tsumanth3@gmail.com>
2024-12-27 12:05:44 +0530

    style update

--- src/script.js
+++ src/script.js
@@ -3,18 +3,21 @@ const date = new Date();
 const timeanddate = document.getElementById('timeanddate');
 const locationData = document.getElementById('location');
 const tempStats = document.getElementById('stats');
+let fetchInterval = 60000 * 10;
 
-const hoursandminutes = () =>{
+const hoursandminutes = (date) =>{
     let minutes =  date.getMinutes();
     let hours = date.getHours();
+   
     hours = hours > 12? hours-12 : hours;
     hours = hours < 10? "0"+hours: hours;
     minutes = minutes < 10? "0"+minutes: minutes;
+
     let time = hours +":"+ minutes;
     return time;
 }
 
-const ampm = () => {
+const ampm = (date) => {
     let hours = date.getHours();
     if(hours < 12){
         return " AM"
@@ -24,37 +27,39 @@ const ampm = () => {
     }
 }
 
-const day = () =>{
+const day = (date) =>{
     const dayarr = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
     return dayarr[date.getDay()];
 }
-const tareekh = () =>{
-    let montharr = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+const nela = (date) =>{
+    let montharr = ["January","February","March","April","May","June","July","August","September","October","November","December"];
     month = montharr[date.getMonth()]
     return date.getDate() +" "+ month;
 }
 
 
 setInterval(() => {
-    timeanddate.innerHTML = `<div id="time" class="font-semibold text-xl">${hoursandminutes()}<span id="ampm">${ampm()}</span>
-                <div class="text-sm font-normal" id="day">${day()}, ${tareekh()}</div>
+    const currentDate = new Date();
+    timeanddate.innerHTML = `<div id="time" class="font-semibold text-xl">${hoursandminutes(currentDate)}<span id="ampm">${ampm(currentDate)}</span>
+                <div class="text-sm font-normal" id="day">${day(currentDate)}, ${nela(currentDate)}</div>
             </div>`
 }, 1000);
 
-const data = fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=Hyderabad&aqi=yes`)
-.then(response => response.json()) // Convert the response to JSON
-.then(data => {
-  console.log(data); // Use the JSON data
-  locationData.innerHTML = `<div id="city"><i class="fa-sharp-duotone fa-solid fa-location-pin"></i> ${data.location.name}</div>
-                        <div id="country">${data.location.country}</div>`
+// setInterval(() => {
+    const data = fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=Hyderabad&aqi=yes`)
+    .then(response => response.json()) 
+    .then(data => {
+    locationData.innerHTML = `<div id="city" class="text-xl font-bold">${data.location.name}</div>
+                            <div id="country"><i class="fa-sharp-duotone fa-solid fa-location-pin"></i> ${data.location.country}</div>`
 
-  tempStats.innerHTML =  `<div id="temperature-box" class="flex flex-col">
-                            <h1 class="font-bold text-3xl text-start" id="temp">${data.current.heatindex_c} &deg;C</h1>
-                            <p id="feelslike" class="text-sm text-start">feels like ${data.current.feelslike_c}&deg;C</p>
-                        </div>
-                            <div id="icon-box">
-                            <img src="${data.current.condition.icon}" alt="" class="object-cover w-full h-full px-3">
-                        </div>`                      
+    tempStats.innerHTML =  `<div id="temperature-box" class="flex flex-col">
+                                <h1 class="font-bold text-3xl text-start" id="temp">${data.current.heatindex_c} &deg;C</h1>
+                                <p id="feelslike" class="text-sm text-start">feels like ${data.current.feelslike_c}&deg;C</p>
+                            </div>
+                                <div id="icon-box">
+                                <img src="${data.current.condition.icon}" alt="" class="object-cover w-full h-auto px-3">
+                            </div>`                      
 
 })
 .catch(error => console.error('Error:', error));
+// },6000);
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'day': "url('https://img.freepik.com/free-photo/sun-flare_74190-1494.jpg')",
+        'day': "url(https://images.unsplash.com/photo-1710241876572-b375dbb54a60?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
         'night': "url(https://images.pexels.com/photos/813269/pexels-photo-813269.jpeg?cs=srgb&dl=pexels-minan1398-813269.jpg&fm=jpg)"
        
       }
